7a34ee5e63b1c8861fef1318c2d028dc5af67047
Hélder Alexandre de Sousa Lima <haslima@gmail.com>
2020-05-20 02:51:32 +0100

    Adicionar arquivos de projeto.

---
+++ JustiCal/Pessoa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustiCal
+{
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        private string firstName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        private string lastName;
+
+        public string OtherNames
+        {
+            get { return otherNames; }
+            set { otherNames = value; }
+        }
+        private string otherNames;
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+        private DateTime birthDate;
+    }
+}
