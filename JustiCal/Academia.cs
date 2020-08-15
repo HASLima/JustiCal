2a156a166d368b448269591dc73ffd83939e92b2
Hélder Alexandre de Sousa Lima <haslima@gmail.com>
2020-08-15 20:16:45 +0100

    Created Academia Class and InAndOut Class Academia Class will own all the lists of Processes and People InAndOut will have the static methods to serialize and desserailize data

---
+++ JustiCal/Academia.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustiCal
+{
+    namespace Model
+    {
+        class Academia
+        {
+            public List<object> Pessoas;
+            public List<object> Processos;
+
+            public Academia()
+            {
+                Pessoas = new List<object>();
+                Processos = new List<object>();
+            }
+        } 
+    }
+}
---
+++ JustiCal/InAndOut.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustiCal
+{
+    namespace Controller
+    {
+        class InAndOut
+        {
+            public static void In()
+            {
+
+            }
+
+            public static void Out()
+            {
+
+            }
+        } 
+    }
+}
